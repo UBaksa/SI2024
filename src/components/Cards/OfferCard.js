d4e7b3a7530d7dbaeacfc19a71408c3165a5dc18
Ujkanovic Bakir <ujkanovicbakir03@gmail.com>
2024-11-27 01:06:04 +0100

    commit

--- src/components/Cards/OfferCard.js
+++ src/components/Cards/OfferCard.js
@@ -10,13 +10,14 @@ import "leaflet-routing-machine";
 export default function OfferCard(){
 
     const routeCoordinates = [
-        [51.505, -0.09],  // London
-        [51.5074, -0.1278], // Westminster
-        [51.7520, -1.2577], // Oxford
-        [52.2053, 0.1218],  // Cambridge
+        [51.505, -0.09],  
+        [51.5074, -0.1278], 
+        [51.7520, -1.2577], 
+        [52.2053, 0.1218],   
+        
     ];
 
-    // Stil linije (boja, širina, itd.)
+    
     const polylineOptions = {
         color: "blue",
         weight: 5,
@@ -29,31 +30,40 @@ export default function OfferCard(){
                     <h3>Flag Ljubljana --- Flag Pristina</h3>
             </div>
             <div className="offer-card-route-company">
-                <img></img>
+                <div className="offer-card-route-company-img">
+               <img src="https://64.media.tumblr.com/c81b84c9d9b7f972ed0d26b30e9c8809/tumblr_inline_n6tbzefUoM1qapiqu.gif"></img>
+                </div>
+                <div className="offer-card-route-company-text">
                 <h3>Baki Trans 2017 DOO</h3>
                 <h5>Country,Post code and City</h5>
+                <p>CarGoo ID:{}</p>
+                </div>
             </div>
             <div className="offer-card-route-user">
             <h3>Kontakt osoba</h3>
                 <div className="offer-card-route-user-info">
-                    <img></img>
-                    <h3>Bakir Ujkanovic</h3>
+                    <div className="offer-card-route-user-info-img">
+                   <img src="https://64.media.tumblr.com/c81b84c9d9b7f972ed0d26b30e9c8809/tumblr_inline_n6tbzefUoM1qapiqu.gif"></img>
+                   </div>
+                   <div className="offer-card-route-user-info-text">
+                    <h2>Bakir Ujkanovic</h2>
                     <p>Language:Srpski,Engleski,Hrvatski,Nemacki</p>
+                    </div>
+                    <div className="offer-card-route-user-info-contact">
+                        <p>Tel: +381621780187</p>
+                        <p>Mail:ujkanovicbakir@gmail.com</p>
+                    </div>
                 </div>
                 <div className="offer-card-route-user-contact"></div>
             </div>
             <div className="offer-card-route-info">
-
             </div>
             <div className="offer-card-route-map">
-            <MapContainer center={[51.505, -0.09]} zoom={7} style={{ height: "100vh", width: "100%" }}>
-            {/* OpenStreetMap TileLayer */}
+            <MapContainer center={[51.505, -0.09]} zoom={7} style={{ height: "30vh", width: "100%",marginTop:"1%" }}>        
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            
-            {/* Ruta prikazana kao Polyline */}
             <Polyline positions={routeCoordinates} pathOptions={polylineOptions} />
         </MapContainer>
             </div>
